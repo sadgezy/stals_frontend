bd7d69a7dcb5b4de51e1f3629185997b3603be80
asd12zxc <fnvargas1@up.edu.ph>
2023-04-26 23:54:14 +0800

    initial homepage implementation

---
+++ lib/UI_parameters.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+// FOR UI Constants
+
+// border radius of the accommodation card that gives it it's rounded edges
+BorderRadius CARD_BORDER_RADIUS = BorderRadius.circular(15);
+
+// font size for headings/titles etc.
+const double FONT_HEADING_SIZE = 16;
+
+// font size for body texts
+const double FONT_BODY_SIZE = 12;
